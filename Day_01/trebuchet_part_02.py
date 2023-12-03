9523d46bf57b5c8ba092985fcd0678dbb13e508e
konstakostov <122868401+konstakostov@users.noreply.github.com>
2023-12-03 12:52:48 +0200

    Updated Day 01, Task 01 so it works properly

--- Day_01/trebuchet_part_02.py
+++ Day_01/trebuchet_part_02.py
@@ -1,63 +1,73 @@
+import re
+#
+# # Max length of the numbers 1-9, spelled
+# max_length = 5
+
+NUMBERS = {
+    'zero': 0,
+    'one': 1,
+    'two': 2,
+    'three': 3,
+    'four': 4,
+    'five': 5,
+    'six': 6,
+    'seven': 7,
+    'eight': 8,
+    'nine': 9
+}
+
+with open('trebuchet_input.txt', 'r') as file:
+    document = file.read().split('\n')
+
 # The total sum of the calibration documents
 calibration_values_sum = 0
 
-# Max length of the numbers 1-9, spelled
-max_length = 5
-
-NUMBERS = [
-    ('zero', '0'),
-    ('one', '1'),
-    ('two', '2'),
-    ('three', '3'),
-    ('four', '4'),
-    ('five', '5'),
-    ('six', '6'),
-    ('seven', '7'),
-    ('eight', '8'),
-    ('nine', '9'),
-]
-
 # Opening file with puzzle input
-with open('trebuchet_input.txt', 'r') as file:
-    for line in file:
-        # Removing '\n' from the end of each line
-        line = line.rstrip()
+for line in document:
+    # Empty string is created to store the found calibration values
+    calibration_value = ""
 
-        # Empty string is created to store the found calibration values
-        calibration_value = ""
-        # Empty list is created to store all found numbers in the documents
-        found_numbers = []
+    # Empty list is created to store all found numbers in the documents
+    found_numbers = []
 
-        # Index to prevent infinite loops when number is added
-        index = None
+    # Empty dictionary to store the index of each found spelled number
+    found_numbers_positions = {}
 
-        for i in range(0, len(line)):
-            if index and i <= index:
-                continue
+    # Iterating through the 'NUMBERS' dictionary to find matches in the current line
+    for num in NUMBERS:
+        # Checking for match for each 'num' in 'NUMBERS
+        match = re.finditer(num, line)
 
-            segment = line[i:i+max_length]
+        # If a match is found the starting index of the match will be stored
+        # as integer in a list, corresponding the spelled number
+        # in the 'found_numbers_positions' dictionary. If there is no match
+        # the value will be empty
+        found_numbers_positions[num] = [m.start() for m in match]
 
-            # Iterating through the data in NUMBERS to find a match
-            for num in NUMBERS:
-                if segment.startswith(num[0]):
-                    # Adding the number in the line, before the spelled out number
-                    line = line[:i] + num[1] + line[i:]
-                    # Increasing index by 1
-                    index = i + 1
-                    break
+    # Iterating through the items in 'found_numbers_positions' dictionary
+    for number, positions_list in found_numbers_positions.items():
+        # Getting the corresponding digit
+        num_value = str(NUMBERS.get(number))
 
-        # Iterating through every character in the line
-        for char in line:
-            # If the character is a number us added to the 'found_numbers' list
-            if char.isnumeric():
-                found_numbers.append(char)
+        # If there were found matches we are going to iterate through
+        # the list of their indexes
+        if positions_list:
+            for position in positions_list:
+                # Replacing the first character of the found spelled number
+                line = line[:position] + num_value + line[position + 1:]
 
-        # The first and last values in the 'found_numbers'
-        # are used to create the 'calibration_value'
-        calibration_value = found_numbers[0] + found_numbers[-1]
+    # Iterating through every character in the list
+    for char in line:
+        # If the current 'char is a digit it is added to the 'found_numbers' list
+        if char.isdigit():
+            found_numbers.append(char)
 
-        # The 'calibration_value' is added to the 'calibration_values_sum'
-        calibration_values_sum += int(calibration_value)
+    # The first and last values in the 'found_numbers'
+    # are used to create the 'calibration_value'
+    calibration_value = found_numbers[0] + found_numbers[-1]
 
-    # The 'calibration_values_sum' is printed to show the result
-    print(calibration_values_sum)
+    # The 'calibration_value' is added to the 'calibration_values_sum'
+    calibration_values_sum += int(calibration_value)
+
+# The 'calibration_values_sum' is printed to show the result
+print(calibration_values_sum)

9075194e9cdab2bc157d7b1134dcd138d2bdfa92
konstakostov <122868401+konstakostov@users.noreply.github.com>
2023-12-06 08:08:22 +0200

    Day 06, Task 01, Task 02, Added comments to both solutions

--- Day_06/wait_for_it_01.py
+++ Day_06/wait_for_it_01.py
@@ -1,35 +1,58 @@
-
+# Opening file containing all races data
 with open('wait_for_it.txt', 'r') as file:
+    # First line contains all the times for all the races
     time = file.readline().strip().split()
+    # Removing the 0 element, which is text
     time.pop(0)
+
+    # First line contains all the record distances for all the races
     distance = file.readline().strip().split()
+    # Removing the 0 element, which is text
     distance.pop(0)
 
+# Error margin counter
+# Value is 1 so no error occurs when multiplying later
 error_margin = 1
 
+# Iterating through every race
 for i in range(0, len(time)):
+    # Time to finish the current race
     time_available = int(time[i])
+    # Record distance to beat
     record_distance = int(distance[i])
 
+    # Time to finish the current race
     current_time = time_available
+
+    # Variable to store all the current race distances
+    # For every possible time
     all_distances = []
 
+    # Iterating through every possible time for the current race
     for j in range(0, time_available + 1):
+        # Speed is equal the current time
         speed = j
+        # Time left to travel
         travel_time = time_available - j
 
+        # Distance passed in the current race
         travel_distance = travel_time * speed
 
+        # Adding the current distance to the 'all_distances' list
         all_distances.append(travel_distance)
 
+    # Variable to count the current race error margin
     race_error = 0
 
+    # Iterating through every distance in the 'all_distances' list
     for d in all_distances:
+        # If current distance is bigger than the record distance
+        # the error margin is increased by 1
         if record_distance < d:
             race_error += 1
 
-    print(race_error)
-
+    # Adding the current race error margin to the total error margin
     error_margin *= race_error
 
+# Printing the final result
 print(error_margin)
--- Day_06/wait_for_it_02.py
+++ Day_06/wait_for_it_02.py
@@ -1,27 +1,48 @@
+# NB! Current solution is not optimized.
+#  It does not run as fast as the solution in Task 01.
+#  Improvement of the algorithm may be applied in the future.
+
+# Opening file containing all races data
 with open('wait_for_it.txt.', 'r') as file:
+    # First line contains all the times for all the races
     time_data = file.readline().strip().split()
+    # Removing the 0 element, which is text
     time_data.pop(0)
+
+    # First line contains all the record distances for all the races
     distance_data = file.readline().strip().split()
+    # Removing the 0 element, which is text
     distance_data.pop(0)
 
+# Variables to store the available time and distance for the only race
 time, distance = '', ''
 
+# Iterating though every element in the 'time_data' and 'distance_data' lists
+# so that the current value can be added to the 'time'/'distance' variable.
 for td in range(len(time_data)):
     time += time_data[td]
     distance += distance_data[td]
 
+# Turning the variables into integers
 time, distance = int(time), int(distance)
 
-
+# Counter of all the possible ways to win this wace
 ways_to_win = 0
 
+# Iterating through every possible race time
 for j in range(0, time + 1):
+    # Speed is equal the current time
     speed = j
+    # Time left to travel
     travel_time = time - j
 
+    # Distance passed in the current race
     travel_distance = travel_time * speed
 
+    # If the current distance is greater than the record distance
+    # the 'ways_to_win is increased by 1
     if travel_distance > distance:
         ways_to_win += 1
 
+# Printing the final result
 print(ways_to_win)
